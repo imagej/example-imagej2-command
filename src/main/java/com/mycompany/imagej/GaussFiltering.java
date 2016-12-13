b1930dedb5ef77b5ac46dabde22d312c3a154011
Yili Zhao <panovr@gmail.com>
2016-12-13 10:03:52 -0600

    Add a minimal ImageJ command plugin example
    Signed-off-by: Curtis Rueden <ctrueden@wisc.edu>

---
+++ src/main/java/com/mycompany/imagej/GaussFiltering.java
@@ -0,0 +1,95 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
+package com.mycompany.imagej;
+
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imagej.ops.OpService;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.RealType;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This example illustrates how to create a minimal ImageJ plugin.
+ */
+
+@Plugin(type = Command.class, menuPath = "Plugins>Gauss Filtering")
+public class GaussFiltering<T extends RealType<T>> implements Command {
+    //
+    // Feel free to add more parameters here...
+    //
+
+    @Parameter
+    private Dataset currentData;
+
+    @Parameter
+    private UIService uiService;
+
+    @Parameter
+    private OpService opService;
+
+    @Override
+    public void run() {
+        final Img<T> image = (Img<T>)currentData.getImgPlus();
+
+        //
+        // Enter image processing code here ...
+        // The following is just a Gauss filtering example
+        //
+        final double[] sigmas = {1.0, 3.0, 5.0};
+
+        List<RandomAccessibleInterval<T>> results = new ArrayList<>();
+
+        for (double sigma : sigmas) {
+            results.add(opService.filter().gauss(image, sigma));
+        }
+
+        // display result
+        for (RandomAccessibleInterval<T> elem : results) {
+            uiService.show(elem);
+        }
+    }
+
+    /**
+     * This main function serves for development purposes.
+     * It allows you to run the plugin immediately out of
+     * your integrated development environment (IDE).
+     *
+     * @param args whatever, it's ignored
+     * @throws Exception
+     */
+    public static void main(final String... args) throws Exception {
+        // create the ImageJ application context with all available services
+        final ImageJ ij = new ImageJ();
+        ij.ui().showUI();
+
+        // ask the user for a file to open
+        final File file = ij.ui().chooseFile(null, "open");
+
+        if (file != null) {
+            // load the dataset
+            final Dataset dataset = ij.scifio().datasetIO().open(file.getPath());
+
+            // show the image
+            ij.ui().show(dataset);
+
+            // invoke the plugin
+            ij.command().run(GaussFiltering.class, true);
+        }
+    }
+
+}
